6374d6f4c2b6381d516d3f1879ed2b0a60305334
Alexandros Monastiriotis <alex.monastiriotis@gmail.com>
2021-02-25 20:07:17 +0200

    isolate logging functionality to different module

--- make_tests.py
+++ make_tests.py
@@ -4,31 +4,11 @@ import os
 import re
 import yaml
 import datetime
-import logging
+
+import log
 
 # Gets or creates a logger
-logger = logging.getLogger(__name__)  
-
-# set log level
-logger.setLevel(logging.INFO)
-
-current_date_and_time = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
-current_date_and_time_string = str(current_date_and_time)
-extension = ".log"
-
-
-if not os.path.exists('logs'):
-    os.makedirs('logs')
-
-file_name =  "logs/" + current_date_and_time_string + extension
-# define file handler and set formatter
-
-file_handler = logging.FileHandler(file_name)
-formatter    = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
-file_handler.setFormatter(formatter)
-
-# add file handler to logger
-logger.addHandler(file_handler)
+logger = log.get(__name__)  
 
 logger.info('******* start ***********')
 
